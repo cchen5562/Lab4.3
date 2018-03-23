565979959fd29690b7bd0a3110b24e0511f2ab27
BT_1N3_18 <BT_1N3_18@BT_1N3_18.bktech.local>
2018-03-23 09:15:20 -0400

    erghe

--- Lab4.3/src/application/Main.java
+++ Lab4.3/src/application/Main.java
@@ -3,21 +3,32 @@ package application;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
 
-
-public class Main extends Application {
+public class Main extends Application 
+{
 	@Override
-	public void start(Stage primaryStage) {
-		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
+	public void start(Stage primaryStage) 
+	{
+		int count = 0;
+		primaryStage.setTitle("FastClicker");
+		Button btn = new Button();
+		btn.setText("Click me~");
+		btn.setOnAction(new EventHandler<ActionEvent>()	//if button gets pressed
+				{
+					@Override
+					public void handle(ActionEvent event) //how do u make a counter for this
+					{
+						count++;	//this is where the button prints out hello world
+					}
+				});
+		StackPane root = new StackPane();
+		root.getChildren().add(btn);
+		primaryStage.setScene(new Scene(root, 300, 250));
+		primaryStage.show();
 	}
 	
 	public static void main(String[] args) {
